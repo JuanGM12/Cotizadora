cafd0ec2481c73edff02c2b1c608cd56bea98901
JuanGM12 <90940947+JuanGM12@users.noreply.github.com>
2022-11-11 19:05:18 -0500

    Add files via upload

---
+++ CotizadoraSAM/Cotizadora/Cotizadora.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Cotizadora
+{
+    partial class Cotizadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cotizadora));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txTiempo = new System.Windows.Forms.TextBox();
+            this.txPeso = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btBorrar = new System.Windows.Forms.Button();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.lbMen = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txExtra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tiempo:";
+            // 
+            // txTiempo
+            // 
+            this.txTiempo.Location = new System.Drawing.Point(63, 123);
+            this.txTiempo.Name = "txTiempo";
+            this.txTiempo.Size = new System.Drawing.Size(100, 20);
+            this.txTiempo.TabIndex = 2;
+            // 
+            // txPeso
+            // 
+            this.txPeso.Location = new System.Drawing.Point(63, 87);
+            this.txPeso.Name = "txPeso";
+            this.txPeso.Size = new System.Drawing.Size(100, 20);
+            this.txPeso.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(187, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(70, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cotizadora SAM3D";
+            // 
+            // btSalir
+            // 
+            this.btSalir.Location = new System.Drawing.Point(300, 198);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(75, 23);
+            this.btSalir.TabIndex = 6;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // btBorrar
+            // 
+            this.btBorrar.Location = new System.Drawing.Point(187, 198);
+            this.btBorrar.Name = "btBorrar";
+            this.btBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btBorrar.TabIndex = 5;
+            this.btBorrar.Text = "Borrar";
+            this.btBorrar.UseVisualStyleBackColor = true;
+            this.btBorrar.Click += new System.EventHandler(this.btBorrar_Click);
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Location = new System.Drawing.Point(63, 198);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btCalcular.TabIndex = 4;
+            this.btCalcular.Text = "Calcular";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // lbMen
+            // 
+            this.lbMen.AutoSize = true;
+            this.lbMen.Location = new System.Drawing.Point(25, 61);
+            this.lbMen.Name = "lbMen";
+            this.lbMen.Size = new System.Drawing.Size(0, 13);
+            this.lbMen.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Extra:";
+            // 
+            // txExtra
+            // 
+            this.txExtra.Location = new System.Drawing.Point(63, 158);
+            this.txExtra.Name = "txExtra";
+            this.txExtra.Size = new System.Drawing.Size(100, 20);
+            this.txExtra.TabIndex = 3;
+            // 
+            // Cotizadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 239);
+            this.Controls.Add(this.txExtra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbMen);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.btBorrar);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txPeso);
+            this.Controls.Add(this.txTiempo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Cotizadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cotizadora SAM3D";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txTiempo;
+        private System.Windows.Forms.TextBox txPeso;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button btBorrar;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.Label lbMen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txExtra;
+    }
+}
+
---
+++ CotizadoraSAM/Cotizadora/Cotizadora.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cotizadora
+{
+    public partial class Cotizadora : Form
+    {
+
+        int pesomaterial = 0;
+        double total = 0;
+        double subtotal = 0;
+        double extra = 0;
+
+        private void btBorrar_Click(object sender, EventArgs e)
+        {
+            txPeso.Text = "";
+            txTiempo.Text = "";
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            if (txPeso.Text == "")
+            {
+                lbMen.Text = "Ingrese un peso";
+            }
+            else {
+
+                /* 
+                 Peso
+                 */
+                if (txTiempo.Text == "")
+                {
+                    lbMen.Text = "Ingrese un tiempo";
+                }
+                else
+                {
+                    pesomaterial = Int32.Parse(txPeso.Text) * 80;
+                if (Int32.Parse(txPeso.Text) >= 0 && Int32.Parse(txPeso.Text) <= 100)
+                {
+                    subtotal = subtotal + 2000;
+                }
+                else if (Int32.Parse(txPeso.Text) >= 101 && Int32.Parse(txPeso.Text) <= 200)
+                {
+                    subtotal = subtotal + 4000;
+                }
+                else if (Int32.Parse(txPeso.Text) >= 201 && Int32.Parse(txPeso.Text) <= 300)
+                {
+                    subtotal = subtotal + 6000;
+                }
+                else if (Int32.Parse(txPeso.Text) >= 301 && Int32.Parse(txPeso.Text) <= 400)
+                {
+                    subtotal = subtotal + 8000;
+                }
+                else if (Int32.Parse(txPeso.Text) >= 401 && Int32.Parse(txPeso.Text) <= 500)
+                {
+                    subtotal = subtotal + 10000;
+                }
+
+                /* 
+                 Tiempo
+                 */
+
+                if(Int32.Parse(txTiempo.Text) > 0 && Int32.Parse(txTiempo.Text) < 7)
+                    {
+                        subtotal = subtotal + 1000;
+                    }
+                else if (Int32.Parse(txTiempo.Text) >= 7 && Int32.Parse(txTiempo.Text) < 14)
+                    {
+                        subtotal = subtotal + 2000;
+                    }
+                 else if (Int32.Parse(txTiempo.Text) >= 14 && Int32.Parse(txTiempo.Text) < 24)
+                    {
+                        subtotal = subtotal + 3000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 24 && Int32.Parse(txTiempo.Text) < 31)
+                    {
+                        subtotal = subtotal + 4000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 31 && Int32.Parse(txTiempo.Text) < 38)
+                    {
+                        subtotal = subtotal + 5000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 38 && Int32.Parse(txTiempo.Text) < 48)
+                    {
+                        subtotal = subtotal + 6000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 48 && Int32.Parse(txTiempo.Text) < 55)
+                    {
+                        subtotal = subtotal + 7000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 55 && Int32.Parse(txTiempo.Text) < 62)
+                    {
+                        subtotal = subtotal + 8000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 62 && Int32.Parse(txTiempo.Text) < 72)
+                    {
+                        subtotal = subtotal + 9000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 72 && Int32.Parse(txTiempo.Text) < 79)
+                    {
+                        subtotal = subtotal + 10000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 79 && Int32.Parse(txTiempo.Text) < 86)
+                    {
+                        subtotal = subtotal + 11000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 86 && Int32.Parse(txTiempo.Text) < 96)
+                    {
+                        subtotal = subtotal + 12000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 96 && Int32.Parse(txTiempo.Text) < 103)
+                    {
+                        subtotal = subtotal + 13000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 103 && Int32.Parse(txTiempo.Text) < 110)
+                    {
+                        subtotal = subtotal + 14000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 110 && Int32.Parse(txTiempo.Text) < 117)
+                    {
+                        subtotal = subtotal + 15000;
+                    }
+                    else if (Int32.Parse(txTiempo.Text) >= 117 && Int32.Parse(txTiempo.Text) < 124 )
+                    {
+                        subtotal = subtotal + 16000;
+                    }
+                    else
+                    {
+                        lbMen.Text = "Error, sobrepasó limite";
+                    }
+
+                    /* 
+                     Fin Tiempo
+                     */
+
+
+                    /* 
+                     Extra
+                     */
+                    if (txExtra.Text == "")
+                    {
+                        extra = 0;
+                    }
+                else if(Int32.Parse(txExtra.Text) >= 0 )
+                    {
+                        extra = Int32.Parse(txExtra.Text);
+                        subtotal = subtotal + extra;
+                    }
+                    else
+                    {
+                        lbMen.Text = "Error!";
+                    }
+
+                    /* 
+                     Fin Extra
+                     */
+
+                    subtotal = subtotal + pesomaterial;
+                    total = subtotal + total;
+
+                    var result = MessageBox.Show("Total de fabricación es: " + total+ "\n\n Total de venta: " +(total* 3)+ "\n\n\n¿Desea ingresar otra cotización?", "Información", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+                    if (result == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+                    else if(result == DialogResult.Yes)
+                    {
+                        total = 0;
+                        subtotal = 0;
+                        extra = 0;
+                        lbMen.Text = "";
+                        txTiempo.Text = "";
+                        txExtra.Text = "";
+                        txPeso.Text = "";
+                        pesomaterial = 0;
+                    }
+                }
+            }
+
+
+            
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public Cotizadora()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
